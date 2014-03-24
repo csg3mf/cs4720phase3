8ebabaa059677a8dd79cb536b42f5752da1e0867
csg3mf <csg3mf>
2014-03-23 21:20:08 -0400

    Deleted unnecessary code

--- server.js
+++ server.js
@@ -19,33 +19,37 @@ http.createServer(function (request, response) {
 	var pathname = url.parse(request.url).pathname;
     var url_parts = url.parse(request.url, true);
     var query = url_parts.query;
-
-    if (pathname == "/") {
-        connection.query('SELECT * FROM ' + 'food' + ';', function (error, rows, fields) {
-        response.writeHead(200, {
-            'Content-Type': 'text/plain'
-        });
-        // Send data as JSON string. 
-        // Rows variable holds the result of the query. 
-        response.write(JSON.stringify(rows));
-        response.end();
-    });
-        
-    	return
-    } else {
-
+     
     pathname_split = pathname.split("/");
+
+
+    // get the passed latitude and longitude values.
+    latitude = pathname_split[0];
+    longitutde = pathname_split[1];
+
+
+
+
+
+
 
     // Query the database. 
     connection.query('SELECT * FROM food' + " WHERE name =" + "'"+ pathname_split[1] + "'"  + ';', function (error, rows, fields) {
         response.writeHead(200, {
             'Content-Type': 'text/plain'
         });
+
+
+
+
+
+
+
         // Send data as JSON string. 
         // Rows variable holds the result of the query. 
         response.write(JSON.stringify(rows));
         response.end();
     });
-}
+
     
 }).listen(process.env.PORT || 8080);
